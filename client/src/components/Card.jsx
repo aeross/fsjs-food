12cea9e798736afbc9fe37970e9f40248f1989c1
aeross <andrewpratama22@gmail.com>
2023-11-15 02:33:06 +1100

    code cleanup

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,9 +6,6 @@ export default function Card({ recipe }) {
             </figure>
             <div>
                 <p className="text-bold text-center my-4">{recipe.name}</p>
-                {/* <p>text 1</p>
-                <p>text 2</p>
-                <p>text 3</p> */}
                 <button className="btn btn-sm my-4">Action</button>
             </div>
         </div>
