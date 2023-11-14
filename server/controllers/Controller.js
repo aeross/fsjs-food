f4db0d3328e20f3aa83ca40cab6b4e37fb2edf60
aeross <andrewpratama22@gmail.com>
2023-11-14 14:54:45 +1100

    CRUD - read done, create on progress

--- server/controllers/Controller.js
+++ server/controllers/Controller.js
@@ -6,12 +6,31 @@ class Controller {
     static async register(req, res, next) {}
 
     static async getRecipes(req, res, next) {
-        await Recipe.findAll({
-            includes: {
-                model: FoodRecipes,
-                includes: Food,
-            },
-        });
+        try {
+            const data = await Recipe.findAll({
+                include: {
+                    model: FoodRecipes,
+                    include: Food,
+                },
+            });
+
+            res.status(200).json(data);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async addRecipe(req, res, next) {
+        try {
+            const { id } = req.loginInfo;
+            const { name, instructions, imageUrl, foods } = req.body;
+            // foods is an array of objects, with keys { name": "cooked rice", "calories",
+            // "protein", "carbohydrate", "fat", "fiber", "sugar", "sodium", "cholesterol", "grams" }
+            const newRecipe = await Recipe.create({ name, instructions, imageUrl, id });
+            await FoodRecipes.create({});
+        } catch (error) {
+            next(error);
+        }
     }
 }
 
